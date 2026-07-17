511c15cfe09e51e9a1c83b9122e881adc41286d9
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2026-07-17 22:01:24 +0600

    learning Python

---
+++ 003a. String/string.py
@@ -0,0 +1,13 @@
+name = "John Doe"
+
+result = name.find("o")
+result2 = name.rfind("o")
+result3 = name.capitalize()
+result4 = name.upper()
+result5 = name.lower()
+result6 = name.isdigit()
+result7 = name.isalpha()
+
+print(result7)
+
+print(help(str))
